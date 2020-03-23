377a20a2afb306918e29a77c717c45ba6369e87a
Stephanie <smendoncasp@gmail.com>
2020-03-23 12:09:13 -0300

    Login

--- proyecto/app/src/main/java/com/example/login/MainActivity.kt
+++ proyecto/app/src/main/java/com/example/login/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.proyectoingsoftware
+package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
--- proyecto/app/src/test/java/com/example/login/ExampleUnitTest.kt
+++ proyecto/app/src/test/java/com/example/login/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.proyectoingsoftware
+package com.example.login
 
 import org.junit.Test
 

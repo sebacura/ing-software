f75745908d67cf7f133678c04b817582132eff23
VictoriaTejera <victoriatejera15@gmail.com>
2020-04-09 18:01:05 -0300

    Pantalla ingresar datos

--- app/src/main/java/pantallas/MainCompletarDatos.java
+++ app/src/main/java/pantallas/MainCompletarDatos.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 
 import com.tananaev.passportreader.R;
 
-public class MainIngresoCedula extends AppCompatActivity {
+public class MainCompletarDatos extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_ingreso_cedula);
+        setContentView(R.layout.activity_main_completar_datos);
     }
 }

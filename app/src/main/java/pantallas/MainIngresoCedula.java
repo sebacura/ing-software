bf6a3826b6b730eb6177fc9783a0c05cb4af2266
Stephanie <smendoncasp@gmail.com>
2020-04-09 16:18:03 -0300

    MainIngresoCedula

---
+++ app/src/main/java/pantallas/MainIngresoCedula.java
@@ -0,0 +1,14 @@
+package pantallas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainIngresoCedula extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ingreso_cedula);
+    }
+}

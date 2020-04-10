c67be292ae717251e87c2589382290fdea825d4d
VictoriaTejera <victoriatejera15@gmail.com>
2020-04-10 20:00:38 -0300

    Notificaciones funcionando

--- app/src/main/java/pantallas/MainCompletarDatos.java
+++ app/src/main/java/pantallas/MainCompletarDatos.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -20,13 +21,30 @@ public class MainCompletarDatos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_completar_datos);
 
-        et1 = (EditText)findViewById(R.id.txt_sueldo);
-      
-        String v1= et1.getText().toString();
-        int s = Integer.parseInt(v1);
+        et1 = findViewById(R.id.txt_sueldo);
+        et2=findViewById(R.id.txt_direcc);
 
-        if(s>10000){
-            Toast.makeText(getApplicationContext(), "k",Toast.LENGTH_LONG ).show();
+    }
+
+    public void btnSig(View view){
+        String v1= et1.getText().toString();
+        String v2= et2.getText().toString();
+        int salario= Integer.parseInt(v1);
+
+        if(v1.isEmpty()){
+            Toast.makeText(this, "Debe ingresar su salario", Toast.LENGTH_LONG).show();
+        }else {
+
+            if (salario < 0) {
+                Toast.makeText(this, "Salario no aceptado", Toast.LENGTH_LONG).show();
+            } else if (salario < 10000) {
+                Toast.makeText(this, "Su salario no es suficiente para solicitar una tarjeta, disculpe", Toast.LENGTH_LONG).show();
+            }
         }
+
+        if(v2.isEmpty()){
+            Toast.makeText(this, "Debe ingresar su dirección", Toast.LENGTH_LONG).show();
+        }
+
     }
 }

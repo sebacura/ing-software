12239ec14314bebc2c9e735311489ae8307fb6af
VictoriaTejera <victoriatejera15@gmail.com>
2020-04-10 12:01:50 -0300

    Notificación

--- app/src/main/java/pantallas/MainCompletarDatos.java
+++ app/src/main/java/pantallas/MainCompletarDatos.java
@@ -3,15 +3,29 @@ package pantallas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.tananaev.passportreader.R;
 
 public class MainCompletarDatos extends AppCompatActivity {
 
+    private EditText et1;
+    private EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_completar_datos);
+
+        et1 = (EditText)findViewById(R.id.txt_sueldo);
+        et2 = (EditText)findViewById(R.id.txt_direcc);
+
+        String v1= et1.getText().toString();
+        int sueldo= Integer.parseInt(v1);
+
+        if(sueldo>10000){
+            Toast.makeText(getApplicationContext(), "Su sueldo no es suficiente para solicitar una tarjeta de crédito, disculpas", 5).show();
+        }
     }
 }

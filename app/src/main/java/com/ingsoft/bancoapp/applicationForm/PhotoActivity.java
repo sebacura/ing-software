681d46836360c63de91145531d7c5bda6b03f3f9
Jose Pedro Chifflet <josepedro@decemberlabs.com>
2020-05-18 19:05:19 -0300

    modals errors

--- app/src/main/java/com/ingsoft/bancoapp/applicationForm/PhotoActivity.java
+++ app/src/main/java/com/ingsoft/bancoapp/applicationForm/PhotoActivity.java
@@ -1,6 +1,8 @@
 package com.ingsoft.bancoapp.applicationForm;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -14,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -24,11 +27,14 @@ import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.ingsoft.bancoapp.R;
 import com.ingsoft.bancoapp.myApplications.StatusActivity;
+import com.ingsoft.bancoapp.products.ProductListActivity;
 import com.ingsoft.bancoapp.tools.Tools;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -39,13 +45,13 @@ public class PhotoActivity extends AppCompatActivity {
 
     View btnTomarFoto;
     Button btnIrFormulario3;
-
     public String KEY_CAMERA_PHOTO_BASE64 = "";
-
+    public Integer numberOfAttemps = 0;
     View loadingLayout;
     View errorMessage;
     private ImageView imageView;
     private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +60,7 @@ public class PhotoActivity extends AppCompatActivity {
         this.imageView = (ImageView)this.findViewById(R.id.imageView1);
 
         loadingLayout = findViewById(R.id.loading_layout);
+
         //Tomar foto desde app
         btnTomarFoto = findViewById(R.id.btnTomarFoto);
         btnTomarFoto.setOnClickListener(new View.OnClickListener() {
@@ -64,7 +71,7 @@ public class PhotoActivity extends AppCompatActivity {
         });
         //Fin tomar foto desde app
 
-        btnIrFormulario3 = (Button)findViewById(R.id.irFormulario3);
+        btnIrFormulario3 = (Button) findViewById(R.id.irFormulario3);
         btnIrFormulario3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,7 +84,6 @@ public class PhotoActivity extends AppCompatActivity {
                 }
             }
         });
-
 
         // bottom nav bar
         BottomNavigationView navigation = findViewById(R.id.navigation);
@@ -190,14 +196,24 @@ public class PhotoActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         try {
                             JSONObject obj = new JSONObject(response);
-                            if (obj.getString("result").equals("true")){
+                            if (obj.getString("result").equals("true")) {
                                 Intent intent = new Intent(getApplicationContext(), ApplicantDetailsActivity.class);
                                 startActivity(intent);
-                                overridePendingTransition(0,0);
+                                overridePendingTransition(0, 0);
                             } else {
                                 findViewById(R.id.loading_layout).setVisibility(View.GONE);
                                 findViewById(R.id.irFormulario3).setVisibility(View.GONE);
-                                Tools.exceptionToast(getApplicationContext(), "Denegado");
+                                new AlertDialog.Builder(PhotoActivity.this)
+                                    .setTitle("Validación fallida.")
+                                    .setMessage("Su solicitud no cumple con nuestros requisitos de validación.\n\nPorfavor contáctese para más información.")
+                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(getApplicationContext(), ProductListActivity.class);
+                                            startActivity(intent);
+                                            overridePendingTransition(0,0);
+                                        }
+                                    }).setCancelable(false).show();
                             }
                         } catch (Throwable tx) {
                             Log.e("My App", "Could not parse malformed JSON: \"" + response + "\"");
@@ -207,8 +223,72 @@ public class PhotoActivity extends AppCompatActivity {
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        loadingLayout.setVisibility(View.GONE);
-                        Tools.exceptionToast(getApplicationContext(), "Service unavailable");
+                        String responseBody = null;
+                        findViewById(R.id.loading_layout).setVisibility(View.GONE);
+                        findViewById(R.id.irFormulario3).setVisibility(View.GONE);
+                        try {
+                            responseBody = new String(error.networkResponse.data, "utf-8");
+                            JSONObject data = new JSONObject(responseBody);
+                            String message = data.optString("msg");
+
+                            if (numberOfAttemps.equals(3)) {
+
+                                new AlertDialog.Builder(PhotoActivity.this)
+                                    .setTitle("Validación de idententidad fallida.")
+                                    .setMessage("Porfavor contáctese para más información.")
+                                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(getApplicationContext(), ProductListActivity.class);
+                                            startActivity(intent);
+                                            overridePendingTransition(0,0);
+                                        }
+                                    }).setCancelable(false).show();
+
+                            } else {
+
+                                if (data.optString("code").equals("NO_FACE")) {
+
+                                    new AlertDialog.Builder(PhotoActivity.this)
+                                            .setTitle("Verificación no exitosa.")
+                                            .setMessage("Porfavor reintente tomándose una foto donde su rostro esté visible.")
+                                            .setPositiveButton("Reintentar", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    numberOfAttemps +=1;
+                                                }
+                                            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(getApplicationContext(), ProductListActivity.class);
+                                            startActivity(intent);
+                                            overridePendingTransition(0,0);
+                                        }
+                                    }).setCancelable(false).show();
+
+                                } else {
+                                    new AlertDialog.Builder(PhotoActivity.this)
+                                            .setTitle("Validación de idententidad fallida.")
+                                            .setMessage("Porfavor reintente nuevamente o contactese para más información.")
+                                            .setPositiveButton("Reintentar", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialog, int which) {
+                                                    numberOfAttemps +=1;
+                                                }
+                                            }).setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            Intent intent = new Intent(getApplicationContext(), ProductListActivity.class);
+                                            startActivity(intent);
+                                            overridePendingTransition(0,0);
+                                        }
+                                    }).setCancelable(false).show();
+                                }
+
+                            }
+                        } catch (UnsupportedEncodingException | JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
         ) {

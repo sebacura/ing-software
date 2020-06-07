01577f4f3e48f993199e6254cf534d5b959733de
VictoriaTejera <victoriatejera15@gmail.com>
2020-06-07 13:02:36 -0300

    py android

---
+++ app/src/main/java/com/ingsoft/bancoapp/applicationForm/blinkDetector/CameraUnavailableException.java
@@ -0,0 +1,9 @@
+package com.ingsoft.bancoapp.applicationForm.blinkDetector;
+/**
+ * Created by muralikrishna on 02/11/17.
+ */
+
+public class CameraUnavailableException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+}
+
